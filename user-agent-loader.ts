585b932e88492b875028bc17867d2e555db9de43
Your Name <you@example.com>
2025-08-08 05:59:17 -0700

    Fix URL in dynamic loader to remove incorrect reference path

--- user-agent-loader.ts
+++ user-agent-loader.ts
@@ -31,7 +31,7 @@ const web_socket_check = async (web_socket_url: string): Promise<boolean> => {
 const dynamic_loader = async () => {
   try {
     const user_agents_json = await custom_fetch(
-      "https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/refs/heads/vanilla/user-agents.json"
+      "https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/user-agents.json"
     );
 
     const user_agents = JSON.parse(user_agents_json);
