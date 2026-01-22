1275632ca9c0dd5d8b4c64bc6710d203acafc396
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-01-23 02:05:27 +0600

    Install nano and neovim in fedora.sh
    Added 'nano' and 'neovim' to the installation list.

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -1,7 +1,7 @@
 # source <(curl -sSL https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/shell-script/fedora.sh)
 rm /etc/yum.repos.d/fedora-cisco-openh264.repo
 dnf upgrade -y
-dnf install -y nodejs python pip fish openssl
+dnf install -y nodejs python pip fish openssl nano neovim
 useradd -m -p "$(openssl passwd -6 123)" retro-boy
 echo "retro-boy ALL=(ALL) ALL" >> /etc/sudoers
 cat <<'EOF' >> /root/.bashrc
