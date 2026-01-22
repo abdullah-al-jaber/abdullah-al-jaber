141f92709d397b91cf48509f32ac3e672e44a1fb
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-01-23 01:34:30 +0600

    Modify Fedora script for package installation
    Updated script source URL and added openssl installation.

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -1,8 +1,8 @@
-# source <(curl -s https://example.com/script.sh)
+# source <(curl -sSL https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/shell-script/fedora.sh)
 rm /etc/yum.repos.d/fedora-cisco-openh264.repo
 dnf upgrade -y
-dnf install -y nodejs python pip fish
-useradd -m -p "$(openssl passwd -6 yourpass)" retro-boy
+dnf install -y nodejs python pip fish openssl
+useradd -m -p "$(openssl passwd -6 123)" retro-boy
 echo "retro-boy ALL=(ALL) ALL" >> /etc/sudoers
 cat <<'EOF' >> /root/.bashrc
 if [ "$(id -u)" -eq 0 ] && [ "$SUDO_USER" = "" ]; then
