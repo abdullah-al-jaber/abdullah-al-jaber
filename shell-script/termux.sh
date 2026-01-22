2fdf537bf4a3deccdb5a378c0b2b4a1bfee97ffe
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-01-23 00:48:34 +0600

    Add files via upload

--- shell-script/termux.sh
+++ shell-script/termux.sh
@@ -6,6 +6,6 @@ pkg install -y proot-distro
 proot-distro install fedora
 proot-distro clear-cache
 curl -L https://abdullah-al-jaber.github.io/asset/archive/termux.zip -o termux.zip
-unzip termux.zip
+unzip -o termux.zip
 rm termux.zip
-exit
+kill -9 $PPID
