60cdecac89ec375156dce73d5c9d94689195967d
Your Name <you@example.com>
2026-03-08 21:08:45 +0600

    OK

--- python-script/chatgpt.py
+++ python-script/chatgpt.py
@@ -1,14 +1,16 @@
 import asyncio
+import rich.traceback
 import playwright.async_api
 import playwright_stealth_plugin
-import rich.traceback
 
+# SCRIPT NEEDS TO RUN IN UBUNTU OR DEBIAN
+# CHAT - LOGIN / ANONYMOUS , TTS - LOGIN ONLY
 rich.traceback.install(show_locals=True)
 
 text_box_selector = "div#prompt-textarea > p"
-chat_button_selector = "button[aria-label='Send prompt']"
+prompt_button_selector = "button[aria-label='Send prompt']"
 voice_button_selector = "button[aria-label='Start Voice']"
-chat_box_selector = "[data-message-author-role='assistant']"
+chat_article_selector = "[data-message-author-role='assistant']"
 more_button_selector = "article[data-turn-id^='request-WEB']:last-of-type button[aria-label='More actions']"
 read_button_selector = "[data-radix-popper-content-wrapper] div[role='menuitem'][aria-label='Read aloud']"
 
@@ -16,16 +18,16 @@ read_button_selector = "[data-radix-popper-content-wrapper] div[role='menuitem']
 async def chat(text: str, page: playwright.async_api.Page) -> str:
     await page.wait_for_selector(text_box_selector)
     await page.fill(text_box_selector, text)
-    await page.wait_for_selector(chat_button_selector)
-    await page.click(chat_button_selector)
+    await asyncio.sleep(min(len(text) * 0.05, 5))
+    await page.wait_for_selector(prompt_button_selector)
+    await page.click(prompt_button_selector)
     await page.wait_for_selector(voice_button_selector)
-    await page.wait_for_selector(chat_box_selector)
-    await asyncio.sleep(2)
-    return (await page.locator(chat_box_selector).nth(-1).inner_text()).strip() or "NO RESPONSE FOUND"
+    await page.wait_for_selector(chat_article_selector)
+    return (await page.locator(chat_article_selector).nth(-1).inner_text()).strip() or "NO RESPONSE FOUND"
 
 
 async def tts(text: str, page: playwright.async_api.Page) -> bytes:
-    await chat(text, page)
+    await chat(f"Please repeat the following text: {text}", page)
     await page.wait_for_selector(more_button_selector)
     await page.click(more_button_selector)
     async with page.expect_response(lambda resp: "backend-api/synthesize" in resp.url and resp.status == 200) as resp_info:
@@ -36,11 +38,6 @@ async def tts(text: str, page: playwright.async_api.Page) -> bytes:
     return audio_bytes
 
 
-def save_audio(audio_bytes: bytes, filename: str):
-    with open(filename, "wb") as file:
-        file.write(audio_bytes)
-
-
 async def main():
     async with playwright.async_api.async_playwright() as pm:
         await playwright_stealth_plugin.async_apply(pm)
@@ -48,17 +45,13 @@ async def main():
         page = await context.new_page()
         try:
             await page.goto("https://chatgpt.com/", wait_until="load")
-            input("Please ensure you are logged in and press enter to continue...")
-            story = await chat("Tell me a story !! Big One...  200 words.", page)
-            rich.print("Story received from ChatGPT:")
-            await chat("Repeat what I say from now on !! Nothing else !!", page)
-            rich.print("TTS starting...")
-            audio_bytes = await tts(story, page)
-            rich.print("Audio received, saving to output.acc")
-            save_audio(audio_bytes, "output.acc")
-            input("Press enter to exit !")
+            rich.print(f'CHAT: {await chat("Tell me joke ! One line !", page)}')
+            # open("audio.aac", "wb").write(await tts("THIS IS A TEST", page))
+            # rich.print("TTS: Play audio.aac to hear the result.")
+            input("[PRESS ENTER TO EXIT]")
         except Exception as error:
             await page.screenshot(path="error_screenshot.png", full_page=True)
+            rich.print("SCREENSHOT SAVED: error_screenshot.png")
             raise error
         finally:
             await context.close()
