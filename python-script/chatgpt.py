0a9f94d5d3cb4901293fc2c20c1f24262910a649
Your Name <you@example.com>
2026-03-12 18:57:48 +0600

    OK

--- python-script/chatgpt.py
+++ python-script/chatgpt.py
@@ -1,38 +1,67 @@
+import sys
 import asyncio
+
+import rich.console
 import rich.traceback
 import playwright.async_api
 import playwright_stealth_plugin
 
-# SCRIPT NEEDS TO RUN IN UBUNTU OR DEBIAN
-# CHAT - LOGIN / ANONYMOUS , TTS - LOGIN ONLY
-rich.traceback.install(show_locals=True)
+console = rich.console.Console()
+rich.traceback.install(console=console, show_locals=True)
+sys.stderr = open("/dev/null", "w")
 
-text_box_selector = "div#prompt-textarea > p"
-prompt_button_selector = "button[aria-label='Send prompt']"
-voice_button_selector = "button[aria-label='Start Voice']"
-chat_article_selector = "[data-message-author-role='assistant']"
-more_button_selector = "article[data-turn-id^='request-WEB']:last-of-type button[aria-label='More actions']"
-read_button_selector = "[data-radix-popper-content-wrapper] div[role='menuitem'][aria-label='Read aloud']"
+url = {
+    "homepage": "https://chatgpt.com",
+    "login": "https://chatgpt.com/auth/login_with",
+    "email_login": "https://auth.openai.com/log-in",
+    "password_login": "https://auth.openai.com/log-in/password",
+    "push_auth": "https://auth.openai.com/push-auth-verification/*",
+}
+
+selector = {
+    "email_input_field": "input[name='email']",
+    "password_input_field": "input[name='current-password']",
+    "submit_button": "button[type='submit']",
+    "prompt_textarea": "div#prompt-textarea > p",
+    "chat_button": "button[aria-label='Send prompt']",
+    "voice_button": "button[aria-label='Start Voice']",
+    "assistant_message": "[data-message-author-role='assistant']",
+    "more_actions_button": "article[data-turn-id^='request-WEB']:last-of-type button[aria-label='More actions']",
+    "read_aloud_button": "[data-radix-popper-content-wrapper] div[role='menuitem'][aria-label='Read aloud']",
+}
 
 
-async def chat(text: str, page: playwright.async_api.Page) -> str:
-    await page.wait_for_selector(text_box_selector)
-    await page.fill(text_box_selector, text)
+async def account_check(page: playwright.async_api.Page) -> bool:
+    await page.goto(url["homepage"], wait_until="networkidle", timeout=60000) if page.url != url["homepage"] else None
+    return any(cookie.get("name") == "oai-gn" and cookie.get("value") == "Jaber" for cookie in await page.context.cookies())
+
+
+async def login(email: str, password: str, page: playwright.async_api.Page):
+    await page.goto(url["login"], wait_until="load")
+    await page.wait_for_url(url["email_login"], wait_until="load")
+    await page.fill(selector["email_input_field"], email)
+    await page.locator(selector["submit_button"]).click()
+    await page.wait_for_url(url["password_login"], wait_until="load")
+    await page.fill(selector["password_input_field"], password)
+    await page.locator(selector["submit_button"]).click()
+    await page.wait_for_url(url["push_auth"], wait_until="load")
+    async with page.expect_navigation(timeout=180000):
+        console.print("Authorize on Your Phone ! Quick !")
+
+
+async def chat(text: str, page: playwright.async_api.Page, timeout: float = 70000) -> str:
+    await page.fill(selector["prompt_textarea"], text)
     await asyncio.sleep(min(len(text) * 0.05, 5))
-    await page.wait_for_selector(prompt_button_selector)
-    await page.click(prompt_button_selector)
-    await page.wait_for_selector(voice_button_selector)
-    await page.wait_for_selector(chat_article_selector)
-    return (await page.locator(chat_article_selector).nth(-1).inner_text()).strip() or "NO RESPONSE FOUND"
+    await page.click(selector["chat_button"])
+    await page.wait_for_selector(selector["voice_button"], timeout=timeout)
+    return (await page.locator(selector["assistant_message"]).nth(-1).inner_text()).strip() or "NO RESPONSE FOUND"
 
 
-async def tts(text: str, page: playwright.async_api.Page) -> bytes:
-    await chat(f"Please repeat the following text: {text}", page)
-    await page.wait_for_selector(more_button_selector)
-    await page.click(more_button_selector)
-    async with page.expect_response(lambda resp: "backend-api/synthesize" in resp.url and resp.status == 200) as resp_info:
-        await page.wait_for_selector(read_button_selector)
-        await page.click(read_button_selector)
+async def tts(text: str, page: playwright.async_api.Page, timeout: float = 70000) -> bytes:
+    await chat(f"REPEAT TEXT: {text}", page, timeout=timeout)
+    await page.click(selector["more_actions_button"])
+    async with page.expect_response(lambda resp: "backend-api/synthesize" in resp.url and resp.status == 200, timeout=timeout) as resp_info:
+        await page.click(selector["read_aloud_button"])
     response = await resp_info.value
     audio_bytes = await response.body()
     return audio_bytes
@@ -41,17 +70,21 @@ async def tts(text: str, page: playwright.async_api.Page) -> bytes:
 async def main():
     async with playwright.async_api.async_playwright() as pm:
         await playwright_stealth_plugin.async_apply(pm)
-        context = await pm.chromium.launch_persistent_context("data", headless=False)
+        context = await pm.chromium.launch_persistent_context("data", headless=True)
         page = await context.new_page()
         try:
-            await page.goto("https://chatgpt.com/", wait_until="load")
-            rich.print(f'CHAT: {await chat("Tell me joke ! One line !", page)}')
-            # open("audio.aac", "wb").write(await tts("THIS IS A TEST", page))
-            # rich.print("TTS: Play audio.aac to hear the result.")
-            input("[PRESS ENTER TO EXIT]")
+            await page.goto(url["homepage"], wait_until="networkidle", timeout=60000)
+            if not await account_check(page):
+                console.print("Proceed to LOGIN !")
+                email = console.input("[ EMAIL ] > ")
+                password = console.input("[ PASSWORD ] > ")
+                await login(email, password, page)
+                assert await account_check(page), "LOGIN FAILURE !"
+            console.print("Welcome to ChatGPT CLI !")
+            await chat("Hello? Are you OK ?", page)
         except Exception as error:
             await page.screenshot(path="error_screenshot.png", full_page=True)
-            rich.print("SCREENSHOT SAVED: error_screenshot.png")
+            console.print("SCREENSHOT: [cyan]error_screenshot.png[/cyan]")
             raise error
         finally:
             await context.close()
