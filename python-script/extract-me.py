b466efbba09c1083c3fc3820ba6cd938c2da5729
Your Name <you@example.com>
2026-03-12 16:58:50 +0600

    OK

--- python-script/extract-me.py
+++ python-script/extract-me.py
@@ -15,7 +15,6 @@ console = rich.console.Console()
 rich.traceback.install(console=console, show_locals=True)
 sys.stderr = open("/dev/null", "w")
 
-blank_line = "\n"
 separator = "●"
 
 
@@ -27,9 +26,8 @@ def download_zip(zip_url: str) -> str:
         rich.progress.TextColumn(separator),
         rich.progress.TimeRemainingColumn(),
     )
-    panel = rich.panel.Panel(progress, title="[ DOWNLOAD ]")
-    group = rich.console.Group(blank_line, panel, blank_line)
-    with rich.live.Live(group, console=console, refresh_per_second=10):
+    panel = rich.panel.Panel(progress, title="[ DOWNLOAD ]", expand=False, width=40)
+    with rich.live.Live(panel, console=console, refresh_per_second=10):
         with httpx.stream("GET", zip_url) as response:
             total = int(response.headers.get("Content-Length", 0))
             task = progress.add_task("# DOWNLOAD #", total=total)
@@ -41,20 +39,21 @@ def download_zip(zip_url: str) -> str:
     return zip_file_name
 
 
-def extract_zip(zip_name: str) -> str:
+def extract_zip(zip_file_name: str) -> str:
     progress = rich.progress.Progress(
         rich.progress.TaskProgressColumn(),
         rich.progress.TextColumn(separator),
         rich.progress.TransferSpeedColumn(),
         rich.progress.TextColumn(separator),
         rich.progress.TimeRemainingColumn(),
+        expand=False,
     )
-    panel = rich.panel.Panel(progress, title="[ EXTRACT ]")
-    group = rich.console.Group(blank_line, panel, blank_line)
-    with rich.live.Live(group, console=console, refresh_per_second=10):
-        with zipfile.ZipFile(zip_name, "r") as zip_file:
+    panel = rich.panel.Panel(progress, title="[ EXTRACT ]", expand=False, width=40)
+    with rich.live.Live(panel, console=console, refresh_per_second=10):
+        with zipfile.ZipFile(zip_file_name, "r") as zip_file:
             task = progress.add_task("# EXTRACT #", total=len(zip_file.infolist()))
-            zip_folder_name = os.path.splitext(zip_name)[0]
+            zip_folder_name = f"{zip_file_name}.extract"
+            os.mkdir(zip_folder_name)
             for info in zip_file.infolist():
                 zip_file.extract(info, path=zip_folder_name)
                 progress.update(task, advance=1)
@@ -63,9 +62,10 @@ def extract_zip(zip_name: str) -> str:
 
 def main():
     zip_url = console.input(" [ ZIP URL ] ")
-    zip_name = download_zip(zip_url)
-    zip_folder = extract_zip(zip_name)
-    console.print(rich.panel.Panel(f"OUTPUT: {zip_folder}", title="[ DONE ]"))
+    zip_file_name = download_zip(zip_url)
+    zip_folder_name = extract_zip(zip_file_name)
+    panel = rich.panel.Panel(f"OUTPUT: [cyan]{zip_folder_name}[/cyan]", title="[ DONE ]", expand=False)
+    console.print(panel)
 
 
 if __name__ == "__main__":
