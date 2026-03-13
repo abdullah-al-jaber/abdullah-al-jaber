d632945e5b20bc5987bfb7cb32e65a5450fda7d5
Your Name <you@example.com>
2026-03-13 07:39:15 +0600

    OK

--- python-script/chatgpt.py
+++ python-script/chatgpt.py
@@ -34,8 +34,19 @@ selector = {
 
 
 async def account_check(page: playwright.async_api.Page) -> bool:
-    if page.url != url["homepage"]: await page.goto(url["homepage"], wait_until="networkidle", timeout=60000)
+    if page.url != url["homepage"]:
+        await page.goto(url["homepage"], wait_until="networkidle", timeout=60000)
     return any(cookie.get("name") == "oai-gn" and cookie.get("value") == "Jaber" for cookie in await page.context.cookies())
+
+
+async def element_click(selector: str, page: playwright.async_api.Page):
+    element = page.locator(selector)
+    await element.wait_for(state="visible")
+    await element.scroll_into_view_if_needed()
+    bounding_box = await element.bounding_box()
+    assert bounding_box, "Bounding Box isn't found !"
+    await page.mouse.click(bounding_box["x"] + bounding_box["width"] / 2, bounding_box["y"] + bounding_box["height"] / 2)
+    await asyncio.sleep(0.75)
 
 
 async def login(email: str, password: str, page: playwright.async_api.Page):
@@ -43,11 +54,11 @@ async def login(email: str, password: str, page: playwright.async_api.Page):
     await page.wait_for_url(url["email_login"], wait_until="load")
     await page.fill(selector["email_input_field"], email)
     await page.locator(selector["submit_button"]).scroll_into_view_if_needed()
-    await page.locator(selector["submit_button"]).click()
+    await element_click(selector["submit_button"], page)
     await page.wait_for_url(url["password_login"], wait_until="load")
     await page.fill(selector["password_input_field"], password)
     await page.locator(selector["submit_button"]).scroll_into_view_if_needed()
-    await page.locator(selector["submit_button"]).click()
+    await element_click(selector["submit_button"], page)
     await page.wait_for_url(url["push_auth"], wait_until="load")
     async with page.expect_navigation(timeout=180000):
         console.print("Authorize on Your Phone ! Quick !")
@@ -56,27 +67,27 @@ async def login(email: str, password: str, page: playwright.async_api.Page):
 async def chat(message_text: str, page: playwright.async_api.Page, timeout: float = 60000) -> str:
     await page.fill(selector["prompt_textarea"], message_text)
     await page.locator(selector["chat_button"]).scroll_into_view_if_needed()
-    await page.click(selector["chat_button"])
+    await element_click(selector["chat_button"], page)
     await page.wait_for_selector(selector["voice_button"], timeout=timeout)
     await page.locator(selector["copy_response_button"]).scroll_into_view_if_needed()
-    await page.click(selector["copy_response_button"])
+    await element_click(selector["copy_response_button"], page)
     return await page.evaluate("navigator.clipboard.readText()") or "NO RESPONSE FOUND"
 
 
 async def tts(text: str, page: playwright.async_api.Page, timeout: float = 80000) -> bytes:
     await chat(f"REPEAT TEXT: {text}", page, timeout=timeout)
     await page.locator(selector["more_actions_button"]).scroll_into_view_if_needed()
-    await page.click(selector["more_actions_button"])
+    await element_click(selector["more_actions_button"], page)
     async with page.expect_response(lambda resp: "backend-api/synthesize" in resp.url and resp.status == 200, timeout=timeout) as resp_info:
         await page.locator(selector["read_aloud_button"]).scroll_into_view_if_needed()
-        await page.click(selector["read_aloud_button"])
+        await element_click(selector["read_aloud_button"], page)
     return await (await resp_info.value).body()
 
 
 async def main():
     async with playwright.async_api.async_playwright() as pm:
         await playwright_stealth_plugin.async_apply(pm)
-        context = await pm.chromium.launch_persistent_context("chatgpt-data", headless=True, permissions=["clipboard-read", "clipboard-write"])
+        context = await pm.chromium.launch_persistent_context("chatgpt-data", headless=False, permissions=["clipboard-read", "clipboard-write"])
         page = await context.new_page()
         try:
             await page.goto(url["homepage"], wait_until="networkidle", timeout=60000)
@@ -90,7 +101,9 @@ async def main():
             while True:
                 console.rule("[USER]", characters="=", style="green")
                 user_text = rich.prompt.Prompt.ask("[PROMPT] > ")
-                if not user_text.startswith("/"): console.print("INVALID ! Must start with '/' !"); continue
+                if not user_text.startswith("/"):
+                    console.print("INVALID ! Must start with '/' !")
+                    continue
                 match user_text.split(maxsplit=1):
                     case ["/chat", message]:
                         chatgpt_text = await chat(message, page)
--- python-script/extract-me.py
+++ python-script/extract-me.py
@@ -60,7 +60,7 @@ def extract_zip(zip_file_name: str) -> str:
 
 
 def main():
-    zip_url = rich.prompt.Prompt.ask(" [ ZIP URL ] ")
+    zip_url = rich.prompt.Prompt.ask(" [ ZIP URL ] > ")
     zip_file_name = download_zip(zip_url)
     zip_folder_name = extract_zip(zip_file_name)
     panel = rich.panel.Panel(f"OUTPUT: [cyan]{zip_folder_name}[/cyan]", title="[ DONE ]", expand=False)
