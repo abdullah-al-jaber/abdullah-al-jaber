43207518166d18f75f47fef387dcf01e8e0af938
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-03-17 01:06:59 +0600

    Update fedora.sh

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -14,6 +14,7 @@ if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
 EOF
+sudo -u retro-boy zsh 'yes n | bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"'
 sudo -u retro-boy zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.local/share/zsh-syntax-highlighting'
 chsh -s /usr/bin/zsh retro-boy
 cat <<'EOF' >> /home/retro-boy/.zshrc
