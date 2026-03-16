ff31cd6ce758cd82f0e5630a7bf25bbbc1ca5e3e
Your Name <you@example.com>
2026-03-16 19:33:11 +0600

    OK

--- shell-script/termux.sh
+++ shell-script/termux.sh
@@ -18,7 +18,7 @@ sed -i \
 	-e 's/^# terminal-margin-vertical=0$/terminal-margin-vertical=5/' \
 	.termux/termux.properties
 chsh -s fish
-fish -c "alias --save fedora='proot-distro login fedora --bind /sdcard:/android --isolated'"
+fish -c "alias --save fedora='proot-distro login fedora --bind /storage/emulated/0/:/android --isolated'"
 fish -c "alias --save clear-history='yes yes | history clear'"
 fish -c "set -U fish_greeting"
 echo >> ~/.config/fish/config.fish
@@ -26,13 +26,5 @@ echo "starship init fish | source" >> ~/.config/fish/config.fish
 starship preset bracketed-segments -o ~/.config/starship.toml
 sed -i '9,11d' ~/.config/starship.toml
 sed -i '2i scan_timeout = 0' ~/.config/starship.toml
-cat << 'EOF' >> ~/.config/starship.toml
-
-[character]
-disabled = false
-success_symbol = '[❯](blue)'
-error_symbol = '[❯](red)'
-
-EOF
 history -c && >~/.bash_history && history -w
 exit
