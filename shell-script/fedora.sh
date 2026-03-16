566025d7ecd8ee759b08b88e7504451a4834625a
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-03-17 00:26:50 +0600

    Update fedora.sh

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -2,7 +2,7 @@
 dnf upgrade -y
 dnf copr -y enable alternateved/eza
 dnf install -y nodejs npm python pip zsh openssl nano neovim glibc-langpack-en ncurses wget eza git
-useradd -m -p '$(openssl passwd -6 123)' retro-boy
+useradd -m -p "$(openssl passwd -6 123)" retro-boy
 echo 'retro-boy ALL=(ALL) ALL' >> /etc/sudoers
 cat <<'EOF' >> ~/.bashrc
 if [ "$(id -u)" -eq 0 ] && [ "$SUDO_USER" = "" ]; then
