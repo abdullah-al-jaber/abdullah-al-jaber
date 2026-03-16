2847fb7b1ab74dda50519caeca7afa23456e1e85
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-03-17 01:24:14 +0600

    Fix command syntax for zsh installation

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -14,7 +14,7 @@ if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
 EOF
-sudo -u retro-boy zsh 'yes n | bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"'
+sudo -u retro-boy zsh -c 'yes n | bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"'
 sudo -u retro-boy zsh -c 'echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> ~/.zshrc'
 chsh -s /usr/bin/zsh retro-boy
 cat <<'EOF' >> /home/retro-boy/.zshrc
