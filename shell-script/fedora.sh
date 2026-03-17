9829addca4c1cd99d998c18d9d4523678d3b1be1
Your Name <you@example.com>
2026-03-17 12:54:14 +0600

    OK

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -16,10 +16,10 @@ if [ -f ~/.bashrc ]; then
 fi
 EOF
 sudo -u retro-boy zsh -c 'yes n | bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"'
+sudo -u retro-boy zsh -c 'echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "zinit ice wait lucid" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "zinit light zdharma-continuum/fast-syntax-highlighting" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "zinit light zsh-users/zsh-autosuggestions" >>~/.zshrc'
-sudo -u retro-boy zsh -c 'echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "eval "$(starship init zsh)" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "alias clear-history=\"yes | history -c\"" >>~/.zshrc'
 chsh -s /usr/bin/zsh retro-boy
