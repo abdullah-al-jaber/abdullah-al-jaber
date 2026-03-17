fe0860775d2f1c8251036a10504b1fbfc2d5024c
Your Name <you@example.com>
2026-03-17 13:35:41 +0600

    OK

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -17,9 +17,10 @@ fi
 EOF
 sudo -u retro-boy zsh -c 'yes n | bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"'
 sudo -u retro-boy zsh -c 'echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >>~/.zshrc'
-sudo -u retro-boy zsh -c 'echo "zinit ice wait lucid" >>~/.zshrc'
-sudo -u retro-boy zsh -c 'echo "zinit light zsh-users/zsh-syntax-highlighting" >>~/.zshrc'
+sudo -u retro-boy zsh -c 'echo "zinit ice depth\"1\" lucid wait\"0\"" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "zinit light zsh-users/zsh-autosuggestions" >>~/.zshrc'
+sudo -u retro-boy zsh -c 'echo "zinit ice depth\"1\" lucid wait\"0\"" >>~/.zshrc'
+sudo -u retro-boy zsh -c 'echo "zinit light zsh-users/zsh-syntax-highlighting" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "eval \"$(starship init zsh)\"" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'echo "alias clear-history=\"history -p\"" >>~/.zshrc'
 sudo -u retro-boy zsh -c 'starship preset bracketed-segments -o ~/.config/starship.toml'
