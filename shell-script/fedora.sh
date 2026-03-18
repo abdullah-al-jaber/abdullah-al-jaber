dba89a89910cc5d36d92ae0f4ffc38d683ccb162
Your Name <you@example.com>
2026-03-18 13:14:31 +0600

    OK

--- shell-script/fedora.sh
+++ shell-script/fedora.sh
@@ -1,26 +1,37 @@
 # source <(curl -sSL https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/shell-script/fedora.sh)
+
 dnf upgrade -y
 dnf install -y nodejs npm python pip fish openssl nano neovim glibc-langpack-en ncurses wget
 useradd -m -p "$(openssl passwd -6 123)" retro-boy
-echo "retro-boy ALL=(ALL) ALL" >>/etc/sudoers
+cat <<'EOF' >>/etc/sudoers
+
+retro-boy ALL=(ALL) ALL
+EOF
 cat <<'EOF' >>~/.bashrc
+
 if [ "$(id -u)" -eq 0 ] && [ "$SUDO_USER" = "" ]; then
   exec su - retro-boy
 fi
 EOF
 cat <<'EOF' >>~/.bash_profile
+
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
 EOF
+
 chsh -s /usr/bin/fish retro-boy
-sudo -u retro-boy fish -c "set -U fish_greeting"
-sudo -u retro-boy fish -c "set -Ux LANG en_US.UTF-8"
-sudo -u retro-boy fish -c "alias --save clear-history='yes yes | history clear'"
+
+sudo -u retro-boy fish -c 'set -U fish_greeting'
+sudo -u retro-boy fish -c 'set -Ux LANG en_US.UTF-8'
+sudo -u retro-boy fish -c 'alias --save clear-history="yes yes | history clear"'
+
 dnf copr enable atim/starship -y
 dnf install -y starship
-sudo -u retro-boy sh -c "echo 'starship init fish | source' >> ~/.config/fish/config.fish"
-sudo -u retro-boy sh -c "starship preset bracketed-segments -o ~/.config/starship.toml"
-sudo -u retro-boy sh -c "sed -i '2i scan_timeout = 0' ~/.config/starship.toml"
+
+sudo -u retro-boy sh -c 'echo "starship init fish | source" >> ~/.config/fish/config.fish'
+sudo -u retro-boy sh -c 'starship preset bracketed-segments -o ~/.config/starship.toml'
+sudo -u retro-boy sh -c 'sed -i "2i scan_timeout = 0" ~/.config/starship.toml'
+
 history -c && >~/.bash_history && history -w
 exit
--- shell-script/termux.sh
+++ shell-script/termux.sh
@@ -1,37 +1,61 @@
 # source <(curl -sSL https://raw.githubusercontent.com/abdullah-al-jaber/abdullah-al-jaber/vanilla/shell-script/termux.sh)
-cd ~
+
+# FUNCTIONS
+echo_lines() {
+	local text=""
+	local flag=1
+	for line in "$@"; do
+		if [ "$flag" -eq 1 ]; then
+			text="$line"
+			flag=0
+		else
+			text+="\n$line"
+		fi
+	done
+	echo -e "\n$text\n"
+}
+
+# PACKAGE
 termux-change-repo
 yes | pkg upgrade
-mv ../usr/etc/motd ../usr/etc/motd.bk
-pkg install -y proot-distro fish starship
+yes | pkg install proot-distro fish starship
+mv ~/../usr/etc/motd ~/../usr/etc/motd.bk
+
+# PROOT
 proot-distro install fedora
 proot-distro clear-cache
+
+# TERMUX
 curl -L https://raw.githubusercontent.com/adi1090x/termux-style/refs/heads/master/colors/smyck.properties -o .termux/colors.properties
 curl -L https://raw.githubusercontent.com/ryanoasis/nerd-fonts/refs/heads/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFont-Regular.ttf -o .termux/font.ttf
-sed -i \
-	-e 's/^# disable-terminal-session-change-toast = true$/disable-terminal-session-change-toast = true/' \
-	-e 's/^# terminal-transcript-rows = 2000$/terminal-transcript-rows = 5000/' \
-	-e 's/^# volume-keys = volume$/volume-keys = volume/' \
-	-e 's/^# use-black-ui = true$/use-black-ui = true/' \
-	-e 's/^# bell-character = ignore$/bell-character = ignore/' \
-	-e 's/^# terminal-margin-horizontal=3$/terminal-margin-horizontal=5/' \
-	-e 's/^# terminal-margin-vertical=0$/terminal-margin-vertical=5/' \
-	.termux/termux.properties
+echo_lines \
+	"# CUSTOM CONFIG #" \
+	"disable-terminal-session-change-toast = true" \
+	"terminal-transcript-rows = 5000" \
+	"volume-keys = volume" \
+	"use-black-ui = true" \
+	"bell-character = ignore" \
+	"terminal-margin-horizontal=5" \
+	"terminal-margin-vertical=5" \
+	>>~/.termux/termux.properties
+
+# SHELL
 chsh -s fish
-fish -c 'alias --save fedora="proot-distro login fedora --bind /storage/emulated/0/:/android --isolated"'
-fish -c 'alias --save clear-history="yes yes | history clear"'
-fish -c 'set -U fish_greeting'
+echo_lines \
+	'alias --save fedora="proot-distro login fedora --bind /storage/emulated/0/:/android --isolated"' \
+	'alias --save clear-history="yes yes | history clear"' \
+	'set -U fish_greeting' |
+	fish
 echo 'starship init fish | source' >>~/.config/fish/config.fish
 starship preset bracketed-segments -o ~/.config/starship.toml
 sed -i '9,11d' ~/.config/starship.toml
 sed -i '2i scan_timeout = 0' ~/.config/starship.toml
-cat <<'EOF' >>~/.config/starship.toml
-
-[character]
-disabled = false
-success_symbol = '[❯](blue)'
-error_symbol = '[❯](red)'
-
-EOF
+echo_lines \
+	'# CUSTOM CONFIG #' \
+	'[character]' \
+	'disabled = false' \
+	'error_symbol = "[❯](red)"' \
+	'success_symbol = "[❯](blue)"' \
+	>>~/.config/starship.toml
 history -c && >~/.bash_history && history -w
 exit
