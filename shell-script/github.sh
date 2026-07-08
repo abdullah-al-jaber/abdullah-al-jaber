86a9bd4f51a8a58e2d535769de233ccade26ba8a
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-07-08 22:23:45 +0600

    OK

--- shell-script/github.sh
+++ shell-script/github.sh
@@ -7,4 +7,3 @@ gh auth setup-git
 git config --global --add safe.directory '/mnt/sdcard/PlayGround/*'
 git config --global user.email 'abdullah.0.al.0.jaber@gmail.com'
 git config --global user.name 'Abdullah Al Jaber'
-
