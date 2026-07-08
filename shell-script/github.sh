b68473c688909775fa6b0c426d6225790fc7a001
Your Name <you@example.com>
2026-07-08 18:32:52 +0600

    OK

--- shell-script/github.sh
+++ shell-script/github.sh
@@ -1,3 +1,5 @@
 sudo dnf install gh git -y
 gh auth login
 git config --global --add safe.directory '/mnt/sdcard/PlayGround/*'
+git config --global user.email "abdullah.0.al.0.jaber@gmail.com"
+git config --global user.name "Abdullah Al Jaber"
